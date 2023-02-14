519c01fc69620ed4b359071bc5d1438100708c63
Eldar Gyulmagomedov <steal.vip@gmail.com>
2023-02-14 15:14:34 +0300

    Описали способ поиска значения в массиве и вывода его индекса, рассмотрели команду *break;*

---
+++ Example010_MetodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿//Поиск значения в массиве и вывод его индекса
+int[] array = { 51, 22, 33, 18, 24, 15, 36, 27, 18,};
+
+int n = array.Length;
+int find = 18;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;// останавливает выполнение . помогает если в искомых значений в массиве больше одного
+    }
+    index++;
+}
